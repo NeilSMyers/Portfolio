cac6fe66056a31bf81da06aba5f7e126fd102aa3
neilmyers <mmyersneil@gmail.com>
2018-12-22 20:55:21 -0700

    map to render multiple titles dynamically

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import PortfolioItem from './portfolio-item';
 
 class PortfolioContainer extends Component {
+  constructor() {
+    super()
+  }
+
+  portfolioItems() {
+    const data = ["Quip", "Eventbrite", "Ministry Safe"]
+
+    return data.map((item, index) => {
+      return <PortfolioItem key={index}/>
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Portfolio Items</h2>
-        <PortfolioItem/>
+        {this.portfolioItems()}
       </div>
     );
   }
