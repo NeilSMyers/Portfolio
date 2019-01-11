7641635a54abc8eae9bd19aebd4e5328eca52b1a
neilmyers <mmyersneil@gmail.com>
2019-01-10 18:12:06 -0700

    login form email and password

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleSubmit = (event) => {
+
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-        <form>
-          <input type="text"/>
-          <input type="password"/>
+
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="email" 
+            name="email" 
+            placeholder="Email" 
+            value={this.state.email} 
+            onChange={this.handleChange}
+          />
+
+          <input 
+            type="password"
+            name="password" 
+            placeholder="Password" 
+            value={this.state.password} 
+            onChange={this.handleChange}  
+          />
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+
         </form>
-        <button>Button</button>
       </div>
     );
   }
