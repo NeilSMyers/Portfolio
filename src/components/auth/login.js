27c390fb25c5736a8f5083e098250ea9bc4c4405
neilmyers <mmyersneil@gmail.com>
2019-01-21 17:23:45 -0700

    auth login

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,13 +7,15 @@ class Login extends Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      errorText: ""
     }
   }
 
   handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      errorText: ""
     })
   }
 
@@ -27,7 +29,15 @@ class Login extends Component {
     },
     {withCredentials: true}
     ).then(response => {
-      console.log(response)
+      if(response.data.status === "created") {
+        this.props.handleSuccessfulAuth()
+      } else {
+        this.setState({ errorText: "Wrong email or password" })
+        this.props.handleUnsuccessfulAuth()
+      }
+    }).catch(error => {
+      this.setState({ errorText: "An error occured" })
+      this.props.handleUnsuccessfulAuth()
     })
     event.preventDefault()
   }
@@ -36,6 +46,8 @@ class Login extends Component {
     return (
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
 
         <form onSubmit={this.handleSubmit}>
           <input 
@@ -65,4 +77,3 @@ class Login extends Component {
 }
 
 export default Login;
-
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -3,13 +3,27 @@ import loginImg from '../../../static/assets/images/login.jpg'
 import Login from '../auth/login';
 
 class Auth extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleSuccessfulAuth = () => {
+    this.props.handleSuccessfulLogin()
+    this.props.history.push("/")
+  }
+
+  handleUnsuccessfulAuth = () => {
+    this.props.handleUnsuccessfulLogin()
+    
+  }
+
   render() {
     return (
       <div className="auth-page-wrapper">
         <div className="left-column" style={{ backgroundImage: `url(${loginImg})` }}/>
         
         <div className="right-column">
-          <Login/>
+          <Login handleSuccessfulAuth={this.handleSuccessfulAuth} handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}/>
         </div>
       </div>
     );
