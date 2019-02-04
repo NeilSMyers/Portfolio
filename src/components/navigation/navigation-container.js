fcaa7646bac2eb74018abf887bbf282bf3997c64
neilmyers <mmyersneil@gmail.com>
2019-02-04 13:54:48 -0700

    font awesome implementation and styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 import axios from "axios"
 import { withRouter } from "react-router"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavigationContainer = props => {
   const dynamicLink = (route, linkText) => {
@@ -44,7 +45,9 @@ const NavigationContainer = props => {
       </div>
       <div className="right-side">
         Neil Myers
-        {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null}
+        {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>
+          <FontAwesomeIcon icon="sign-out-alt"/>
+        </a> : null}
       </div>
     </div>
   );
