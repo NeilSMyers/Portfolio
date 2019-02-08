164e75ad6366a177be3a141a23a1daebd0f8359b
neilmyers <mmyersneil@gmail.com>
2019-02-08 13:25:40 -0700

    ternary for image editing

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -53,7 +53,10 @@ class PortfolioForm extends Component {
         url: url || "",
         editMode: true,
         apiUrl: `https://neilmyers.devcamp.space/portfolio/portfolio_items/${id}`,
-        apiAction: "patch"
+        apiAction: "patch",
+        thumb_image: thumb_image_url || "",
+        banner_image: banner_image_url || "",
+        logo: logo_url || ""
       })
     }
   }
@@ -207,6 +210,10 @@ class PortfolioForm extends Component {
         </div>
         
         <div className="image-uploaders three-column">
+          {this.state.thumb_image && this.state.editMode ?
+            <img src={this.state.thumb_image}/> :
+              
+          
           <DropzoneComponent
             ref={this.thumbRef}
             config={this.componentConfig()} 
@@ -215,7 +222,7 @@ class PortfolioForm extends Component {
           >
             <div className="dz-message">Thumbnail</div>
           </DropzoneComponent>
-
+            }
           <DropzoneComponent 
             ref={this.bannerRef}
             config={this.componentConfig()} 
