fd6b1796dd08ba86c0aab7f9f2ec839a13ec3217
neilmyers <mmyersneil@gmail.com>
2019-02-15 16:30:19 -0700

    rendered blog items

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from "react-router-dom"
+import axios from "axios"
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super()
 
+    this.state = {
+      blogItems: []
+    }
+  }
+
+  componentWillMount() {
+    this.getBlogItems()
+  }
+
+  getBlogItems = () => {
+    axios.get("https://neilmyers.devcamp.space/portfolio/portfolio_blogs", { withCredentials: true })
+    .then(response => {
+      this.setState({
+        blogItems: response.data.portfolio_blogs
+      })
+    }).catch(error => {
+      console.log("getBlogItems", error)
+    })
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <h1>{blogItem.title}</h1>
+    })
+
+    return (
       <div>
-        <Link to="/about-me">More About Me</Link>
+        <h2>Blog</h2>
+        <div>{blogRecords}</div>
+        
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog
