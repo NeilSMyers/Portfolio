a855e4e9fc7372a620c52f063e8e0592bd0b0d1e
neilmyers <mmyersneil@gmail.com>
2019-02-19 17:44:00 -0700

    calling single blog item from api

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import axios from "axios"
 
 class BlogDetail extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    }
+  }
+
+  getBlogItem = () => {
+    axios.get(`https://neilmyers.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`)
+    .then(response => {
+
+    }).catch( error => {
+      console.log("getBlogItem", error)
+    })
+  }
+
+  componentDidMount() {
+    this.getBlogItem()
+  }
+
   render() {
     return (
       <div>
