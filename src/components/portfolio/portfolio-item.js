17de14161fa0a61c847f6b4db82a338e69176313
neilmyers <mmyersneil@gmail.com>
2019-03-19 16:08:58 -0600

    portfolio detail styles, filter full func

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom"
 
 class PortfolioItem extends Component {
   constructor(props) {
@@ -14,26 +15,28 @@ class PortfolioItem extends Component {
   }
 
   handleMouseLeave() {
-    this.setState({ portfolioItemClass: " "})
+    this.setState({ portfolioItemClass: ""})
   }
   
   render() {
     const { id, description, thumb_image_url, logo_url, url } = this.props.item
     return (
-      <div className="portfolio-item-wrapper" 
-        onMouseEnter={() => this.handleMouseEnter()} 
-        onMouseLeave={() => this.handleMouseLeave()}
-      >
-        <div className={`portfolio-img-background ${this.state.portfolioItemClass}`} 
-          style={{ backgroundImage: `url(${thumb_image_url})` }}
-        />
-        <div className="img-text-wrapper">
-          <div className="logo-wrapper">
-            <img src={logo_url}/>
+      <Link to={`/portfolio/${id}`}>
+        <div className="portfolio-item-wrapper" 
+          onMouseEnter={() => this.handleMouseEnter()} 
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <div className={`portfolio-img-background ${this.state.portfolioItemClass}`} 
+            style={{ backgroundImage: `url(${thumb_image_url})` }}
+          />
+          <div className="img-text-wrapper">
+            <div className="logo-wrapper">
+              <img src={logo_url}/>
+            </div>
+            <div className="subtitle">{description}</div>
           </div>
-          <div className="subtitle">{description}</div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
